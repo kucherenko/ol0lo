84ae2afaf14406bb007c32ed27338722031c7bc8
Andrey Kucherenko <kucherenko.andrey@gmail.com>
2023-07-12 15:32:23 +0300

    Delete logger_interface.go

--- src/logger_interface.go
+++
@@ -1,9 +0,0 @@
-package logger
-
-// Logger defines the interface for logging
-// Implementations of this interface will provide
-// different log output options
-
-type Logger interface {
-    Log(message string)
-}
