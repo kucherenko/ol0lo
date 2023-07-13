a7ce2bcc92ae78c5f0d591000c2f299b344cb75b
Andrey Kucherenko <kucherenko.andrey@gmail.com>
2023-07-13 14:23:32 +0300

    Delete jest.config.js

--- jest.config.js
+++
@@ -1,7 +0,0 @@
-// jest.config.js
-module.exports = {
-  preset: '@nuxt/test-utils',
-  collectCoverage: true,
-  collectCoverageFrom: ['**/*.vue', '!**/node_modules/**'],
-  coverageDirectory: 'coverage',
-}
